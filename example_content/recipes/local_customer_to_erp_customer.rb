0b1fa5431e3f07232c319e8de36211d3c27bad8e
James Christie and Alex Williams <dev+JamesChristie+robovirtuoso@acumenbrands.com>
2013-08-02 15:59:06 -0500

    @wip Added TODO for future work

--- example_content/recipes/local_customer_to_erp_customer.rb
+++ example_content/recipes/local_customer_to_erp_customer.rb
@@ -10,4 +10,14 @@ Alchemist::RecipeBook.write LocalCustomer, ErpCustomer do
 
   transfer :email, :email_address
 
+  # TODO(jchristie+robovirtuoso) Implement this new ritual, possibly with a
+  # better name
+  # alter_member :user_events, :event_log do |member, user_events|
+  #   'member' inside this block is the Array of EventLog objects without an
+  #   accessor on ErpCustomer
+  #
+  #   member = result.public_send(result_method)
+  #   block.call(member, source_value)
+  # end
+
 end
